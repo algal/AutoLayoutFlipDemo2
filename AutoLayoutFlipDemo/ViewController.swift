03ba736c815d0b43b41e18edc4396cb455c0e8ca
Alexis Gallagher <alexis@alexisgallagher.com>
2014-10-21 21:08:08 -0700

    non show/hide version

--- AutoLayoutFlipDemo/ViewController.swift
+++ AutoLayoutFlipDemo/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet weak var viewA: UILabel!
-  @IBOutlet weak var viewB: UILabel!
 
+  weak var viewB: UILabel!
 
   @IBAction func handleTap(sender: AnyObject) {
+    
+    let destinationView = UILabel(frame: self.viewA.frame)
+    destinationView.font = self.viewA.font
+    destinationView.text = "B"
+    
     UIView.transitionFromView(self.viewA,
-      toView: self.viewB,
+      toView: destinationView,
       duration: NSTimeInterval(0.6),
-      options: .TransitionFlipFromLeft | UIViewAnimationOptions.ShowHideTransitionViews,
-      completion: nil)
+      options: .TransitionFlipFromLeft,
+      completion: { (success:Bool) -> () in
+        NSLog("self.view.subviews=%@", self.view.subviews)
+        self.viewB = destinationView
+        self.view.addConstraint(NSLayoutConstraint(item: destinationView, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0))
+        self.view.addConstraint(NSLayoutConstraint(item: destinationView, attribute: .CenterY, relatedBy: .Equal, toItem: self.view, attribute: .CenterY, multiplier: 1, constant: 0))
+      }
+    )
+    
+    NSLog("self.view.subviews=%@", self.view.subviews)
+    
   }
 }
 
